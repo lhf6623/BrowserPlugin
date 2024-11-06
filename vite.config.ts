7a831a0184a320dd04c79203c05ee635cb6d5900
lhf6623 <1336955219@qq.com>
2024-11-06 16:12:46 +0800

    fix: 把 README 描述图片从打包中过滤

--- vite.config.ts
+++ vite.config.ts
@@ -8,60 +8,62 @@ import { rmSync } from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		Crx({ manifest }),
-		UnoCSS(),
-		React(),
-		{
-			// 删除打包后的vite文件夹
-			name: "remove-vite-folder",
-			closeBundle() {
-				const viteFolder = resolve(__dirname, "dist/.vite");
-				rmSync(viteFolder, { recursive: true, force: true });
-			},
-		},
-	],
-	server: {
-		host: true,
-		port: 5173,
-		strictPort: true,
-		hmr: {
-			port: 5173,
-		},
-	},
-	resolve: {
-		alias: {
-			"@": resolve(__dirname, "./src"),
-			"@opt": resolve(__dirname, "./src/options"),
-			"@pages": resolve(__dirname, "./src/options/pages"),
-		},
-	},
-	build: {
-		rollupOptions: {
-			input: {
-				popup: resolve(__dirname, "popup.html"),
-				options: resolve(__dirname, "options.html"),
-			},
-			output: {
-				chunkFileNames: "assets/js/[name]-[hash].js",
-				entryFileNames: "assets/js/[name]-[hash].js",
-				assetFileNames: "assets/[ext]/[name]-[hash].[ext]",
-				manualChunks(id: string) {
-					const checkId = "/node_modules/";
-					if (id.includes(checkId)) {
-						const ids = id.split(checkId);
-						if (ids.length) {
-							const lastIds = ids.at(-1);
-							const regex = /^(\w+)/;
-							const match = lastIds!.match(regex);
+  plugins: [
+    Crx({ manifest }),
+    UnoCSS(),
+    React(),
+    {
+      // 删除打包后的vite文件夹
+      name: "remove-vite-folder",
+      closeBundle() {
+        const viteFolder = resolve(__dirname, "dist/.vite");
+        rmSync(viteFolder, { recursive: true, force: true });
+      },
+    },
+  ],
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      port: 5173,
+    },
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+      "@opt": resolve(__dirname, "./src/options"),
+      "@pages": resolve(__dirname, "./src/options/pages"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, "popup.html"),
+        options: resolve(__dirname, "options.html"),
+      },
+      // 把 README 描述图片从打包中过滤
+      external: ["README_IMG/**"],
+      output: {
+        chunkFileNames: "assets/js/[name]-[hash].js",
+        entryFileNames: "assets/js/[name]-[hash].js",
+        assetFileNames: "assets/[ext]/[name]-[hash].[ext]",
+        manualChunks(id: string) {
+          const checkId = "/node_modules/";
+          if (id.includes(checkId)) {
+            const ids = id.split(checkId);
+            if (ids.length) {
+              const lastIds = ids.at(-1);
+              const regex = /^(\w+)/;
+              const match = lastIds!.match(regex);
 
-							if (match && match[1]) {
-								return match[1];
-							}
-						}
-					}
-				},
-			},
-		},
-	},
+              if (match && match[1]) {
+                return match[1];
+              }
+            }
+          }
+        },
+      },
+    },
+  },
 });
